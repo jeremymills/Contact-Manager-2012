6ad905cc48615c7c8d2914272c1bc7ddb12a8f05
Jeremy Mills <jeremy.mills89@gmail.com>
2012-12-11 21:24:03 -0500

    set up database connectivity in bootstrap & authentication within config

--- config.php
+++ config.php
@@ -1,3 +1,19 @@
 <?php
 
 defined('IN_APPLICATION') or exit;
+
+/*
+ * Return array for mySQL injection
+ *
+ *Change db_host to your mysql host name
+ *Change db_user to your mysql user name
+ *Change db_pass to your mysql password
+ *Change db_name to your mysql database name
+ */
+
+return array(
+	'db_host' => '127.0.0.1',
+	'db_user' => 'root',
+	'db_pass' => 'password',
+	'db_name' => 'lamp1_project2_jjrm'
+);
