4649ee70a4a84092d07f575062e3753a249cb796
Jeremy Mills <jeremy.mills89@gmail.com>
2012-12-11 21:31:17 -0500

    Additionaly added seperate file for dealing with mysql connection

---
+++ config.init.php
@@ -0,0 +1,48 @@
+<?php
+
+defined('IN_APPLICATION') or exit;
+
+ini_set('display_errors', 'on');
+error_reporting(E_ALL | E_STRICT);
+// ==================================
+// load MySQL database authentication
+$config = require_once ROOT_PATH . 'config.php';
+
+// =========================================================================
+// adding in simple copy and paste work from in class project work
+// normalize / check the array this it is an array for "extra" measures here
+if( is_array($config) ) {
+
+	// =======================
+	// connect to database ...
+	try {
+		$mysqli = new mysqli( $config['db_host'], $config['db_user'], $config['db_pass'], $config['db_name'] );
+		if( $mysqli->connect_error ) {
+			trigger_error('An error has occured while connecting to the datbase. Error: [ ' . $mysqli->connect_errno . ' ] ' . $mysqli->connect_error, E_USER_ERROR);
+		}//end if
+	} catch( Exception $exception ) {
+		print 'Can not connect to the database.<br />Error: '.$exception->getMessage() . '<br />';
+	}
+	
+	// =======================================================
+	// delete password from $config array for security reasons
+	unset($config['db_pass']);
+	
+}//end if
+else {
+	trigger_error('Application configuration details appear to be invalid', E_USER_ERROR);
+}
+
+// ======================================================
+// close the connection to the database shutdown function
+function shutdown_func() {
+	global $mysqli;
+
+	if( $mysqli instanceof mysqli ) {
+		$mysqli->close();
+	}
+	$mysqli = null;
+}
+// ============================================================
+// register shutdown function to ensure garbage clean up is set
+register_shutdown_function('shutdown_func');
